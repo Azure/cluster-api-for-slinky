74eced4bee466f2b86038163300b5ad306d5a07c
Zheyu Shen <zheyushen@microsoft.com>
2025-04-11 22:00:00 +0000

    initial support for CAPD

--- projects/test/roles/sync/files/run.py
+++ projects/test/roles/sync/files/run.py
@@ -1,10 +1,57 @@
 #!/usr/bin/python3.11
 import os
+import subprocess
 import tempfile
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 ping_yml_path = os.path.join(current_dir, 'ping.yml')
 
 with tempfile.TemporaryDirectory() as tempdir:
+    # copy the /runner/.kube/config file to tempdir
+    kube_config_path = os.path.join(tempdir, 'config')
+    subprocess.run(
+        [
+            'cp',
+            '/runner/.kube/config',
+            kube_config_path,
+        ],
+        stdout=subprocess.PIPE,
+    )
+    # replace all occurences of 127.0.0.1 with host.docker.internal
+    # TODO: this is a kind-specific hack, generalize this
+    with open(kube_config_path, 'r') as file:
+        content = file.read()
+    
+    modified_content = content.replace('127.0.0.1', 'host.docker.internal')
+    
+    with open(kube_config_path, 'w') as file:
+        file.write(modified_content)
+
     # run ansible-playbook cli
-    pass
+    result = subprocess.run(
+        [
+            'ansible-playbook',
+            ping_yml_path,
+            '-i',
+            'localhost,',
+            # TODO: find a way to avoid hardcoding this
+            '-e',
+            'ansible_python_interpreter=/usr/bin/python3.11',
+        ],
+        env=dict(
+            os.environ,
+            # TODO: this is a kind-specific hack to get around the cert issue for host.docker.internal
+            K8S_AUTH_VERIFY_SSL="false",
+            K8S_AUTH_KUBECONFIG=kube_config_path,
+        ),
+        stdout=subprocess.PIPE,
+
+    )
+    # result = subprocess.run(
+    #     [
+    #         'ls',
+    #         '-al',
+    #         '/runner/.kube',
+    #     ], stdout=subprocess.PIPE,
+    # )
+    print(result.stdout.decode('utf-8'))
--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-kind create cluster --config=kind-config.yaml
+envsubst < kind-config.yaml | kind create cluster --config -
 kubectl cluster-info --context kind-kind
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 helm repo add awx-operator https://ansible-community.github.io/awx-operator-helm/
 helm install my-awx-operator awx-operator/awx-operator -n awx --create-namespace -f awx.yaml
+export CLUSTER_TOPOLOGY=true
+clusterctl init --infrastructure docker
+kubectl apply -f capi-quickstart.yaml
+clusterctl get kubeconfig capi-quickstart > capi-quickstart.kubeconfig
+# https://cluster-api.sigs.k8s.io/clusterctl/developers#fix-kubeconfig-when-using-docker-desktop-and-clusterctl
+# Point the kubeconfig to the exposed port of the load balancer, rather than the inaccessible container IP.
+sed -i -e "s/server:.*/server: https:\/\/$(docker port capi-quickstart-lb 6443/tcp | sed "s/0.0.0.0/127.0.0.1/")/g" ./capi-quickstart.kubeconfig
+# CAPD nodes won't be ready until we install CNI
+kubectl --kubeconfig=./capi-quickstart.kubeconfig apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.3/manifests/calico.yaml
 # kubectl get secret awx-admin-password -n awx -o jsonpath="{.data.password}" | base64 --decode ; echo
