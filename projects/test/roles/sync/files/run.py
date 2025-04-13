b48e264390282b1be9b19c449fd211cabf78d720
Zheyu Shen <zheyushen@microsoft.com>
2025-04-13 21:12:31 +0000

    gather CAPD node information from k8s

--- projects/test/roles/sync/files/run.py
+++ projects/test/roles/sync/files/run.py
@@ -2,9 +2,11 @@
 import os
 import subprocess
 import tempfile
+import yaml
+import json
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-ping_yml_path = os.path.join(current_dir, 'ping.yml')
+sync_yml_path = os.path.join(current_dir, 'sync.yml')
 
 with tempfile.TemporaryDirectory() as tempdir:
     # copy the /runner/.kube/config file to tempdir
@@ -28,15 +30,18 @@ with tempfile.TemporaryDirectory() as tempdir:
         file.write(modified_content)
 
     # run ansible-playbook cli
+
     result = subprocess.run(
         [
             'ansible-playbook',
-            ping_yml_path,
+            sync_yml_path,
             '-i',
             'localhost,',
             # TODO: find a way to avoid hardcoding this
             '-e',
             'ansible_python_interpreter=/usr/bin/python3.11',
+            '-e',
+            f'output_path={tempdir}',
         ],
         env=dict(
             os.environ,
@@ -45,13 +50,33 @@ with tempfile.TemporaryDirectory() as tempdir:
             K8S_AUTH_KUBECONFIG=kube_config_path,
         ),
         stdout=subprocess.PIPE,
-
     )
-    # result = subprocess.run(
-    #     [
-    #         'ls',
-    #         '-al',
-    #         '/runner/.kube',
-    #     ], stdout=subprocess.PIPE,
-    # )
-    print(result.stdout.decode('utf-8'))
+
+    with open(os.path.join(tempdir, 'output.yaml'), 'rb') as temp_file:
+        # parse temp_file as yaml
+        data = yaml.safe_load(temp_file)
+        # for dev purposes on CAPD clusters, we take the following simplifying assumptions:
+        # - only 1 node under the MachineDeployment/MachineSet, acting as the slurm scheduler node
+        # - only 1 `MachinePool`, under which all nodes are slurm compute nodes
+        scheduler_nodes = [node for node in data['resources'] if any(r['kind'] == 'MachineSet' for r in node['metadata']['ownerReferences'])]
+        compute_nodes = [node for node in data['resources'] if any(r['kind'] == 'MachinePool' for r in node['metadata']['ownerReferences'])]
+        ip = lambda x: [addr['address'] for addr in x['status']['addresses'] if addr['type'] == 'ExternalIP'][0]
+        hostname = lambda x: [addr['address'] for addr in x['status']['addresses'] if addr['type'] == 'Hostname'][0]
+        # https://docs.ansible.com/ansible-tower/latest/html/administration/scm-inv-source.html#custom-dynamic-inventory-scripts
+        output_json = {
+            "_meta": {
+                "hostvars": {
+                    hostname(node): {"ansible_host": ip(node)} for node in scheduler_nodes + compute_nodes
+                }
+            },
+            "all": {
+                "children": ["compute", "scheduler"],
+            },
+            "scheduler": {
+                "hosts": [hostname(node) for node in scheduler_nodes],
+            },
+            "compute": {
+                "hosts": [hostname(node) for node in compute_nodes],
+            },
+        }
+        print(json.dumps(output_json, indent=2))
--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ kubectl cluster-info --context kind-kind
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 helm repo add awx-operator https://ansible-community.github.io/awx-operator-helm/
 helm install my-awx-operator awx-operator/awx-operator -n awx --create-namespace -f awx.yaml
+# kubectl get secret awx-admin-password -n awx -o jsonpath="{.data.password}" | base64 --decode ; echo
 export CLUSTER_TOPOLOGY=true
 clusterctl init --infrastructure docker
 kubectl apply -f capi-quickstart.yaml
@@ -13,4 +14,3 @@ clusterctl get kubeconfig capi-quickstart > capi-quickstart.kubeconfig
 sed -i -e "s/server:.*/server: https:\/\/$(docker port capi-quickstart-lb 6443/tcp | sed "s/0.0.0.0/127.0.0.1/")/g" ./capi-quickstart.kubeconfig
 # CAPD nodes won't be ready until we install CNI
 kubectl --kubeconfig=./capi-quickstart.kubeconfig apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.3/manifests/calico.yaml
-# kubectl get secret awx-admin-password -n awx -o jsonpath="{.data.password}" | base64 --decode ; echo
