8b86fb117d4845545ceb470a3f786118d7da7d27
Zheyu Shen <zheyushen@microsoft.com>
2026-07-07 16:52:23 +0000

    Fix AKS machine pool resource options merge

--- pulumi/stacks/workload_cluster/workload_cluster_infrastructure_aks.py
+++ pulumi/stacks/workload_cluster/workload_cluster_infrastructure_aks.py
@@ -350,11 +350,13 @@ class AKSWorkloadClusterInfrastructure(pulumi.ComponentResource):
                     autoscaling_bounds=pool.autoscaling_bounds,
                 ),
                 opts=pulumi.ResourceOptions.merge(
-                    child_opts(depends_on=[azure_managed_control_plane]),
-                    pulumi.ResourceOptions(
-                        custom_timeouts=pulumi.CustomTimeouts(
-                            delete=_AKS_DELETE_TIMEOUT
-                        )
+                    pulumi.ResourceOptions.merge(
+                        child_opts(depends_on=[azure_managed_control_plane]),
+                        pulumi.ResourceOptions(
+                            custom_timeouts=pulumi.CustomTimeouts(
+                                delete=_AKS_DELETE_TIMEOUT
+                            )
+                        ),
                     ),
                     delete_with_cluster_opts,
                 ),
